49dd0ea5451ea1b7db1c399a53131d9381ad001c
EslamElsebaey <eslamelsebaey.11@gmail.com>
2022-06-09 13:59:14 +0200

    7 create

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-// selecting variables 
+// selecting variables
 
 let day = document.getElementById("day");
 let date = document.getElementById("date");
 let city = document.getElementById("city");
 let num = document.getElementById("num");
 let cloud = document.getElementById("cloud");
-let weatherObj = {}; 
+let weatherObj = {};
 let scndDay = document.getElementById("scndDay");
 let scndDayMaxTemp = document.getElementById("scndDayMaxTemp");
 let scndDayMinTemp = document.getElementById("scndDayMinTemp");
@@ -22,106 +22,93 @@ let searchInput = document.getElementById("searchInput");
 let tryToWrite = document.getElementById("tryToWrite");
 let find = document.getElementById("find");
 
-
-
-// search country 
+// search country
 
 find.addEventListener("click", function () {
-  if(searchInput.value.length < 4){
-   tryToWrite.classList.remove("d-none");
-   searchInput.value = "";
-  }else{
-     tryToWrite.classList.add("d-none");
-     getWeather(searchInput.value);
+  if (searchInput.value.length < 4) {
+    tryToWrite.classList.remove("d-none");
+    searchInput.value = "";
+  } else {
+    tryToWrite.classList.add("d-none");
+    getWeather(searchInput.value);
   }
-    
- 
-   
 });
- 
 
+// get weather
 
-// get weather 
-
-async function getWeather (term){
-    let response = await fetch(
-      `https://api.weatherapi.com/v1/forecast.json?key=1c38be8040b44f4ca57114247220806&q=${term}&days=3`
-    );
-    let result = await response.json();
-   weatherObj = result;
-   displayWeather();
+async function getWeather(term) {
+  let response = await fetch(
+    `https://api.weatherapi.com/v1/forecast.json?key=1c38be8040b44f4ca57114247220806&q=${term}&days=3`
+  );
+  let result = await response.json();
+  weatherObj = result;
+  displayWeather();
 }
 
 getWeather("cairo");
 
-
-
-
-
 //  display Weather
-function displayWeather(){
-const weekday = ["Sunday","Monday","Tuesday","Wednesday", "Thursday","Friday", "Saturday"];
-const d = new Date();
-let today = weekday[d.getDay()];
-day.innerHTML = today;
+function displayWeather() {
+  const weekday = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const d = new Date();
+  let today = weekday[d.getDay()];
+  day.innerHTML = today;
 
-const yearMonth = [ "January", "February", "March","April", "May", "June","July","August", "September", "October", "November","December",];
-const m = new Date() ; 
-let month = yearMonth[m.getMonth()];
-let dayInMonth = m.getDate();
-date.innerHTML = `${dayInMonth}${month}`;
-city.innerHTML = weatherObj.location.name;
-num.innerHTML = `${Math.round(weatherObj.current.temp_c) }<sup>o</sup>C <img src="${weatherObj.current.condition.icon}" alt="">`;
-cloud.innerHTML = weatherObj.current.condition.text;
-scndDayWord = weekday[weekday.indexOf(today) + 1];
-thrdDayWord = weekday[weekday.indexOf(today) + 2];
-scndDayWeather();
-thrdDayWeather();
+  const yearMonth = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const m = new Date();
+  let month = yearMonth[m.getMonth()];
+  let dayInMonth = m.getDate();
+  date.innerHTML = `${dayInMonth}${month}`;
+  city.innerHTML = weatherObj.location.name;
+  num.innerHTML = `${Math.round(
+    weatherObj.current.temp_c
+  )}<sup>o</sup>C <img src="${weatherObj.current.condition.icon}" alt="">`;
+  cloud.innerHTML = weatherObj.current.condition.text;
+  scndDayWord = weekday[weekday.indexOf(today) + 1];
+  thrdDayWord = weekday[weekday.indexOf(today) + 2];
+  scndDayWeather();
+  thrdDayWeather();
 }
 
-
-
-// second day weather 
+// second day weather
 
 function scndDayWeather() {
-    scndDay.innerHTML = scndDayWord;
+  scndDay.innerHTML = scndDayWord;
   scndDayIcon.src = weatherObj.forecast.forecastday[1].day.condition.icon;
-  scndDayCloud.innerHTML = weatherObj.forecast.forecastday[1].day.condition.text;
+  scndDayCloud.innerHTML =
+    weatherObj.forecast.forecastday[1].day.condition.text;
   scndDayMaxTemp.innerHTML = `${weatherObj.forecast.forecastday[1].day.maxtemp_c}<sup>o</sup>C`;
   scndDayMinTemp.innerHTML = `${weatherObj.forecast.forecastday[1].day.mintemp_c}<sup>o</sup>C`;
 }
 
-
 // third day weather
 
 function thrdDayWeather() {
-    thrdDay.innerHTML = thrdDayWord;
+  thrdDay.innerHTML = thrdDayWord;
   thrdDayIcon.src = weatherObj.forecast.forecastday[2].day.condition.icon;
-  thrdDayCloud.innerHTML =weatherObj.forecast.forecastday[2].day.condition.text;
+  thrdDayCloud.innerHTML =
+    weatherObj.forecast.forecastday[2].day.condition.text;
   thrdDayMaxTemp.innerHTML = `${weatherObj.forecast.forecastday[2].day.maxtemp_c}<sup>o</sup>C`;
   thrdDayMinTemp.innerHTML = `${weatherObj.forecast.forecastday[2].day.mintemp_c}<sup>o</sup>C`;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
